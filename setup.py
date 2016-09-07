8f9d28eb2a41ba77385fe429ce292dff5a407c96
zpace <zpace21@gmail.com>
2016-09-07 15:33:47 -0500

    more setup info

--- setup.py
+++ setup.py
@@ -1,5 +1,10 @@
 from setuptools import setup
 
+
+def readme():
+    with open('README.md') as f:
+        return f.read()
+
 setup(name='zaphodIFU',
       version='0.1',
       description='Fake IFU generator for MaNGA spectral fitting diagnosis',
@@ -8,4 +13,7 @@ setup(name='zaphodIFU',
       author_email='zpace@astro.wisc.edu',
       license='MIT',
       packages=['fake'],
+      install_requires=[
+          'numpy'],
+      include_package_data=True,
       zip_safe=False)
