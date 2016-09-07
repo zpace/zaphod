a148a268a32ecb1f8d896b69a1ada8328ed88a69
zpace <zpace21@gmail.com>
2016-09-07 15:27:06 -0500

    packaging framework

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='zaphodIFU',
+      version='0.1',
+      description='Fake IFU generator for MaNGA spectral fitting diagnosis',
+      url='http://github.com/zpace/zaphod',
+      author='Zach Pace',
+      author_email='zpace@astro.wisc.edu',
+      license='MIT',
+      packages=['fake'],
+      zip_safe=False)
